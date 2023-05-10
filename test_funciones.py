efe3d437117e6a2b8119e28b90d59ae246b2fc9a
Ibusteros <ibusterosr193@gmail.com>
2023-05-10 09:24:00 +0200

    test flake8 y workflow

---
+++ test_funciones.py
@@ -0,0 +1,11 @@
+import funciones
+
+
+def test_suma():
+    assert 2 == funciones.suma(1, 1)
+    assert 4 == funciones.suma(2, 2)
+
+
+def test_resta():
+    assert 2 == funciones.resta(4, 2)
+    assert 0 == funciones.resta(2, 2)
