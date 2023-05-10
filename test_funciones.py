601987d73eea07b7e091d5cf6962ab3821f77c95
Ibusteros <ibusterosr193@gmail.com>
2023-05-10 09:53:49 +0200

    branch f8 and pytest

--- test_funciones.py
+++ test_funciones.py
@@ -1,6 +1,5 @@
 import funciones
 import requests
-import json
 
 
 def test_suma():
@@ -25,7 +24,7 @@ def test_api():
 
     list_names = requests.get(f'{url_base}{names}')
     last_name = list_names.json()[-1]
-    params_c = {'file' : last_name}
+    params_c = {'file':last_name}
     data_c = requests.get(f'{url_base}{filedata}', params_c)
 
     assert 200 == list_names.status_code
