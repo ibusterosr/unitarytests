b7f8488881d36ad9b6f50e19b0895c8a94c4c3bf
Ibusteros <ibusterosr193@gmail.com>
2023-05-10 09:51:28 +0200

    prueba api f8 y pytest

--- test_funciones.py
+++ test_funciones.py
@@ -1,4 +1,6 @@
 import funciones
+import requests
+import json
 
 
 def test_suma():
@@ -14,3 +16,17 @@ def test_resta():
 def test_suma_resta():
     assert 2 == funciones.suma(1, 1)
     assert 0 == funciones.resta(1, 1)
+
+
+def test_api():
+    url_base = 'https://api-mycloudstorage-gpxkblrpvq-ew.a.run.app'
+    names = '/allnames'
+    filedata = '/data'
+
+    list_names = requests.get(f'{url_base}{names}')
+    last_name = list_names.json()[-1]
+    params_c = {'file' : last_name}
+    data_c = requests.get(f'{url_base}{filedata}', params_c)
+
+    assert 200 == list_names.status_code
+    assert 200 == data_c.status_code
