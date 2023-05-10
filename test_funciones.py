222269d3214bd8166e110ab5789649c50fc745d6
Ibusteros <ibusterosr193@gmail.com>
2023-05-10 09:40:06 +0200

    cambios en funciones y f8

--- test_funciones.py
+++ test_funciones.py
@@ -9,3 +9,8 @@ def test_suma():
 def test_resta():
     assert 2 == funciones.resta(4, 2)
     assert 0 == funciones.resta(2, 2)
+
+
+def test_suma_resta():
+    assert 2 == funciones.suma(1,1)
+    assert 0 == funciones.resta(1,1)
